d5d9f5863ed2e479c9d2d7297561d786f4a246dd
unknown <hrushikesh7702@gmail.com>
2024-05-10 20:09:13 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -21,10 +21,10 @@ function Home(){
  
 function Assemble(){
     return(
-        <BrowserRouter>
+        <BrowserRouter basename="/reactzoro">
         <Nav/>
         <Routes>
-          <Route path='/' element={<Home />}/>
+          <Route path="/reactzoro" element={<Home />}/>
           <Route path="/profile/:data/:dis/:img" element={<Profile />}/>
           <Route path="/watch/:data/:dis/:img" element={<Watch />}/>
           <Route path="/login" element={<Login />}/>
